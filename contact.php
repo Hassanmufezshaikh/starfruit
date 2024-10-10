7e2f51437ff0ac66c020680ca15545bf8e7d2242
Hassan Mufez Shaikh <132350748+Hassanmufezshaikh@users.noreply.github.com>
2024-10-10 12:26:55 +0530

    Update contact.php

--- contact.php
+++ contact.php
@@ -55,7 +55,7 @@ if(isset($_POST['submit'])){
 
             //Content
             $mail->isHTML(true);                                  //Set email format to HTML
-            $mail->Subject = '🌟Star Fruits';
+            $mail->Subject = 'Star Fruits';
             $mail->Body    = "Sender Name - $name <br> Sender Email - $email <br> Sender Phone Number - $phone <br> Sender Message - $message";
 
             $mail->send();
